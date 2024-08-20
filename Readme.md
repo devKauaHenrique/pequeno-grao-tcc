6be1f90103cbdfefdbb608e78699d49b03339c4d
unknown <Aluno@L3T11.acad.etecpoa>
2024-08-19 21:29:21 -0300

    pequeno grao

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Trabalho de Conclusão de Curso (TCC)
+
+A Equipe Pequeno Grão, composta por Júlia Moura, Kauã Henrique, Márcio Amélio, Rebeca Lobato e Rámon fez esse site com muito amor e muito carinho para todas as mamães e todos os papais de primeira viagem!
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'rosa-principal': "#FFA9C5",
+        'rosa-secundario': "#621D39",
+        'rosa-terciario': "#E88DAE",
+        'azul-principal': "#2971D3",
+        'azul-secundario': "#003191",
+        "custom-transparent": "rgba(0, 0, 0, 0.4)"
+      },
+      backgroundImage: {
+        'home': "url(/assets/home.jpg)"
+      },
+      height: {
+        'screen-minus-header-1': 'calc(100vh - 5rem)',
+      },
+      fontFamily: {
+        'poppins-bold': ['Poppins-bold', 'sans-serif'],
+        'poppins-medium': ['Poppins-medium', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+        'poppins-semibold': ['Poppins-semibold', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
