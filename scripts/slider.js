96645b3b0b14885c8594b2033c73cabe4bdd7e15
unknown <devKauaHenrique.com>
2024-08-26 22:36:08 -0300

    pequeno-grao

---
+++ scripts/slider.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    effect: "fade",
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+        loop: true,
+    }
+});
