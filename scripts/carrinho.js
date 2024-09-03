f0fabd65bd59b239d5508a840dc21df96ef78a99
devKauaHenrique <devKauaHenrique.com>
2024-09-03 17:58:14 -0300

    pequeno-grao

---
+++ scripts/carrinho.js
@@ -0,0 +1,171 @@
+const carrinho = document.getElementById("carrinho");
+const cartBtn = document.getElementById("cart-btn");
+const cartBtn2 = document.getElementById("cart-btn-2");
+const cartModal = document.getElementById("cart-modal");
+const cartItemsContainer = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const checkoutBtn = document.getElementById("checkout-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const cartCounter = document.getElementById("cart-count");
+const cartCounter2 = document.getElementById("cart-count-2");
+const addressInput = document.getElementById("address");
+const addressWarn = document.getElementById("address-warn");
+
+let cart = [];
+
+//Abrir o carrinho no mobile
+cartBtn.addEventListener("click", function () {
+    cartModal.style.display = "flex"
+})
+
+//Abrir o carrinho no desktop
+cartBtn2.addEventListener("click", function () {
+    updateCartModal();
+    cartModal.style.display = "flex"
+})
+
+//Fechar o carrinho quando clicar fora
+cartModal.addEventListener("click", function (event) {
+    if (event.target === cartModal) {
+        cartModal.style.display = "none"
+    }
+})
+
+//Fechar o modal quando clica no "fechar"
+closeModalBtn.addEventListener("click", function () {
+    cartModal.style.display = "none"
+})
+
+carrinho.addEventListener("click", function (event) {
+    let parentButton = event.target.closest(".add-to-cart-btn")
+
+    if (parentButton) {
+        const name = parentButton.getAttribute("data-name");
+        const price = parseFloat(parentButton.getAttribute("data-price"));
+
+        //Adicionar ao carrinho
+        addToCart(name, price)
+    }
+})
+
+//Função para adicionar no carrinho
+function addToCart(name, price) {
+    const existingItem = cart.find(item => item.name === name)
+
+    //Se já existe um item igual no carrinho
+    if (existingItem) {
+        existingItem.quantity += 1;
+    }
+    else {
+        cart.push({
+            name,
+            price,
+            quantity: 1,
+        })
+    }
+
+    updateCartModal();
+}
+
+//Atualiza o carrinho
+function updateCartModal() {
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
+
+        cartItemElement.innerHTML = `
+        <div class="flex items-center justify-between"> 
+            <div> 
+                <p class="font-bold">${item.name}</p>
+                <p>Quantidade: ${item.quantity}</p>
+                <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+            </div>
+
+            <button class="remove-from-cart-btn bg-red-500 hover:bg-red-800 text-white font-medium rounded px-4 py-1" data-name="${item.name}">
+                Remover
+            </button>
+
+        </div>
+        `
+
+        total += item.price * item.quantity;
+
+        cartItemsContainer.appendChild(cartItemElement);
+    })
+
+    cartTotal.textContent = total.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
+
+    cartCounter.innerHTML = `(${cart.length})`;
+    cartCounter2.innerHTML = `(${cart.length})`;
+}
+
+//Para remover o item do carrinho
+cartItemsContainer.addEventListener("click", function(event){
+    if(event.target.classList.contains("remove-from-cart-btn")){
+        const name = event.target.getAttribute("data-name");
+
+        removeItemCart(name);
+    }
+})
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name === name);
+
+    if(index !== -1){
+        const item = cart[index];
+
+        if(item.quantity > 1){
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+
+        cart.splice(index, 1);
+        updateCartModal();
+    }
+}
+
+//Pegar o Endereço
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+})
+
+//Finalizar o pedido
+checkoutBtn.addEventListener("click", function(){
+    if(cart.length === 0) return;
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden");
+        addressInput.classList.add("border-red-500");
+        return;
+    }
+
+    //Enviar pedido para o whatsapp
+const cartItems = cart.map((item) => {
+    return (
+        `(${item.quantity}) ${item.name} - Preço: R$${item.price} </br>
+        Endereço: ${addressInput.value}
+        -----------------------------
+        `
+    )
+}).join("")
+
+    const message = encodeURIComponent(cartItems)
+    const phone = "5511953736849"
+
+    window.open(`https://wa.me/${phone}?text=${message}`, "_blank");
+
+    cart = [];
+    updateCartModal();
+})
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,8 @@ module.exports = {
         'azul-principal': "#2971D3",
         'azul-secundario': "#003191",
         "custom-transparent": "rgba(0, 0, 0, 0.4)",
-        "cinza": "#D9D9D9",
-        "cinza-escuro": "#3F3F3F",
+        "salmao": "#D4AA78",
+        "salmao-escuro": "#ad7d44",
       },
       backgroundImage: {
         'home': "url(/assets/home.jpg)"
@@ -20,10 +20,7 @@ module.exports = {
         'screen-minus-header-1': 'calc(100vh - 5rem)',
       },
       fontFamily: {
-        'poppins-bold': ['Poppins-bold', 'sans-serif'],
-        'poppins-medium': ['Poppins-medium', 'sans-serif'],
         'poppins': ['Poppins', 'sans-serif'],
-        'poppins-semibold': ['Poppins-semibold', 'sans-serif'],
       }
     },
   },
